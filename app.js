01c111099ea70be1ace4a82f5e27020a5d734baf
Kent Lazaga <arvinkent121816@gmail.com>
2020-06-28 17:35:11 +0800

    Added Image Scraper

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const request = require('request-promise');
+const requestPromise = require('request-promise');
+const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const { resolve } = require('path');
 
-const urls = [
-  'https://www.imdb.com/title/tt8946378/?ref_=hm_fanfav_tt_1_pd_fp1',
-  'https://www.imdb.com/title/tt5753856/?ref_=hm_fanfav_tt_2_pd_fp1',
+const movies = [
+  { 
+    url: 'https://www.imdb.com/title/tt8946378/?ref_=hm_fanfav_tt_1_pd_fp1', 
+    id: 'knive_out',
+  },
+  {
+    url: 'https://www.imdb.com/title/tt5753856/?ref_=hm_fanfav_tt_2_pd_fp1', 
+    id: 'dark',
+  },
 ];
 
 (async () => {
   const moviesData = [];
 
-  for (let movie of urls) {
-    const html = await request({
-      url: movie,
+  for (let movie of movies) {
+    const html = await requestPromise({
+      url: movie.url,
       gzip: true,
     });
   
@@ -23,10 +31,12 @@ const urls = [
     let rating = $('span[itemprop="ratingValue"]').text();
   
     let duration = $('div.title_wrapper > div > time').text();
+
+    let thumbnail = $('div.poster > a > img').attr('src');
   
     let releasedDate = $('.txt-block:nth-child(6)').text();
   
-    let genres = [];
+    const genres = [];
     
     $('a[href^="/search/title?genres"]').each((_index, element) => {
       let genre = $(element).text();
@@ -42,13 +52,26 @@ const urls = [
       title,
       rating,
       duration,
+      thumbnail,
       releasedDate,
       uniqueGenres,
     });
 
+    let file = fs.createWriteStream(`${movie.id}.jpg`);
+
+    await new Promise((resolve, reject) => {
+      let stream = request({
+        uri: thumbnail,
+        gzip: true,
+      })
+      .pipe(file)
+      .on('finish', () => {
+        console.log('finished scraping image...', stream.path);
+        resolve();
+      })
+    });
+
     fs.writeFileSync('./data.json', JSON.stringify(moviesData), 'utf-8');
   }
-
-  console.log(moviesData);
 })();
 
